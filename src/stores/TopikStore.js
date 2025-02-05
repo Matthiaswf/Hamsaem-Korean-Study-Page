ad15f9ad5ea1835262f944dab039b078e1ab3a42
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-02-05 18:11:04 +0900

    Topik 1 Data Added and Working With Pinia

--- src/stores/TopikStore.js
+++ src/stores/TopikStore.js
@@ -3,11 +3,14 @@ import topik1Data from '@/assets/topikData/topik1.json';
 
 export const useTopikStore = defineStore('topik', {
   state: () => ({
-    topik1: topik1Data, // Load JSON data directly into state
+    topik1: [], // Ensures reactivity
   }),
-  getters: {
+  actions: {
+    loadTopik1() {
+      this.topik1 = topik1Data; // Load data into state
+    },
     getTopik1() {
-      return this.topik1;
+      return this.topik1; // Function to return state
     },
   },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Topik 1</h1>
+    <div v-for="t in topik1" :key="t.id">
+      <p>{{ t.id }}</p>
+      <p>{{ t.english }}</p>
+      <p>{{ t.korean }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import { useTopikStore } from '@/stores/TopikStore';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
 
-<style scoped></style>
+export default {
+  setup() {
+    const topikStore = useTopikStore();
+    const { topik1 } = storeToRefs(topikStore); // Ensures reactivity
+
+    onMounted(() => {
+      topikStore.loadTopik1(); // Load data into state
+      console.log(topikStore.getTopik1()); // Log the state to verify it's working
+    });
+
+    return { topik1 };
+  },
+};
+</script>
