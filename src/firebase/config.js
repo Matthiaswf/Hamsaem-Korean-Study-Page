cd55c386e4e58a0aefd2c638f8959a29ef45a3ea
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-02-05 16:15:19 +0900

    Firebase Config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDqNAPoPWKKogc_0o_9829cwQx9hqxG-GE',
+  authDomain: 'hamsaem-658c8.firebaseapp.com',
+  projectId: 'hamsaem-658c8',
+  storageBucket: 'hamsaem-658c8.firebasestorage.app',
+  messagingSenderId: '153205682134',
+  appId: '1:153205682134:web:996b0fb9125e09530bfe77',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Initialize Services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
